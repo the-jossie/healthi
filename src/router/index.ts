b116bb335ffaf415be5903d2f71ad7260a7ea5ee
Dev Jo <idowuayanfeoluwa@gmail.com>
2024-10-01 19:49:23 +0100

    wip: login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
-import DashboardView from '@/views/dashboard/DashboardView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import DashboardView from '@/views/dashboard/DashboardView.vue'
+import LoginView from '@/views/auth/login/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: DashboardView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
     }
   ]
 })
